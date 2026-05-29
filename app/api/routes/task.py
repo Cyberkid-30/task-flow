8e7b2e17fb998f4575a42b0a689c5b67180b4ea4
Cyberkid-30 <janphandoh@gmail.com>
2026-05-29 21:34:06 +0000

    Refactor task route handlers to use task_service methods directly

--- app/api/routes/task.py
+++ app/api/routes/task.py
@@ -2,13 +2,7 @@ from fastapi import APIRouter, HTTPException, status
 from sqlalchemy.exc import DatabaseError
 from schemas.task_schema import TaskCreate, TaskUpdate, TaskResponse
 from api.deps import Current_User_Dependency, DBSession
-from services.task_service import (
-    create_task,
-    fetch_my_tasks,
-    fetch_task,
-    update_task,
-    delete_task,
-)
+from services import task_service
 
 task_router = APIRouter(prefix="/api/tasks", tags=["Tasks"])
 
@@ -16,7 +10,7 @@ task_router = APIRouter(prefix="/api/tasks", tags=["Tasks"])
 @task_router.post("/", response_model=TaskResponse, status_code=status.HTTP_201_CREATED)
 def create(request: TaskCreate, db: DBSession, user: Current_User_Dependency):
     try:
-        task = create_task(request, db, user)
+        task = task_service.create_task(request, db, user)
     except DatabaseError:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -28,7 +22,7 @@ def create(request: TaskCreate, db: DBSession, user: Current_User_Dependency):
 @task_router.get("/", response_model=list[TaskResponse])
 def get_my_tasks(db: DBSession, user: Current_User_Dependency):
     try:
-        tasks = fetch_my_tasks(db, user)
+        tasks = task_service.fetch_my_tasks(db, user)
     except DatabaseError:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -40,7 +34,7 @@ def get_my_tasks(db: DBSession, user: Current_User_Dependency):
 @task_router.get("/{id}", response_model=TaskResponse)
 def get_task(id: str, db: DBSession, user: Current_User_Dependency):
     try:
-        task = fetch_task(id, db, user)
+        task = task_service.fetch_task(id, db, user)
     except DatabaseError:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -52,7 +46,7 @@ def get_task(id: str, db: DBSession, user: Current_User_Dependency):
 @task_router.put("/{id}", response_model=TaskResponse | dict)
 def update(id: str, task: TaskUpdate, db: DBSession, user: Current_User_Dependency):
     try:
-        updated_task = update_task(id, task, db, user)
+        updated_task = task_service.update_task(id, task, db, user)
     except DatabaseError:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -64,7 +58,7 @@ def update(id: str, task: TaskUpdate, db: DBSession, user: Current_User_Dependen
 @task_router.delete("/{id}")
 def delete(id: str, db: DBSession, user: Current_User_Dependency):
     try:
-        delete_task(id, db, user)
+        task_service.delete_task(id, db, user)
     except DatabaseError:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
