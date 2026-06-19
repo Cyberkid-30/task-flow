429144713e519d0b744e38291deb42798191a787
Cyberkid-30 <janphandoh@gmail.com>
2026-06-19 13:46:03 +0000

    Update env.py to use settings for database URL and add migration for created_at and updated_at columns to datetime

--- alembic/env.py
+++ alembic/env.py
@@ -4,14 +4,14 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from app.core.config import Config
+from app.core.config import settings
 from app.core.database import Base
 from app.models import task_model, user_model
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", Config.SYNC_DATABASE_URL)
+config.set_main_option("sqlalchemy.url", settings.SYNC_DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
